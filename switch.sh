035fd411963b35d2c59eca49757ff1b69f711b59
Amir Khazaieli <Amir Khazaieli>
2017-08-31 20:22:44 -0700

    added kernel folder fix

--- switch.sh
+++ switch.sh
@@ -25,32 +25,32 @@ ${CURDIR}/ethminer/ethminer -G -S us-east.ethash-hub.miningpoolhub.com:12020 -O 
 #monero
 #${CURDIR}/monero/xmrMiner/build/xmrMiner -r 0 -R 4 -l 16x54 -o stratum+tcp://us-east.cryptonight-hub.miningpoolhub.com:12024 -O ${NAME}:x -D
 (PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
-         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --shaders ${SHADERS} --event-on idle --event-quit true -k cryptonight -o stratum+tcp://us-east.cryptonight-hub.miningpoolhub.com:12024 -u ${NAME} -p x) \
+         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --shaders ${SHADERS} -K kernel --event-on idle --event-quit true -k cryptonight -o stratum+tcp://us-east.cryptonight-hub.miningpoolhub.com:12024 -u ${NAME} -p x) \
                   1> >(tee >(grep -q "Press any key to exit" && kill $(cat $PIDFILE)) >&1) \
               & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
 
 (PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
-         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --shaders ${SHADERS} --event-on idle --event-quit true -k vanilla -o stratum+tcp://hub.miningpoolhub.com:12019 -u ${NAME} -p x) \
+         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --shaders ${SHADERS} -K kernel --event-on idle --event-quit true -k vanilla -o stratum+tcp://hub.miningpoolhub.com:12019 -u ${NAME} -p x) \
                   1> >(tee >(grep -q "Press any key to exit" && kill $(cat $PIDFILE)) >&1) \
               & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
 (PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
-         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --shaders ${SHADERS} --event-on idle --event-quit true -k groestlcoin -o stratum+tcp://hub.miningpoolhub.com:12004 -u ${NAME} -p x) \
+         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --shaders ${SHADERS} -K kernel --event-on idle --event-quit true -k groestlcoin -o stratum+tcp://hub.miningpoolhub.com:12004 -u ${NAME} -p x) \
                   1> >(tee >(grep -q "Press any key to exit" && kill $(cat $PIDFILE)) >&1) \
               & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
 (PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
-         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --shaders ${SHADERS} --event-on idle --event-quit true -k myriadcoin-groestl -o stratum+tcp://hub.miningpoolhub.com:12005 -u ${NAME} -p x) \
+         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --shaders ${SHADERS} -K kernel --event-on idle --event-quit true -k myriadcoin-groestl -o stratum+tcp://hub.miningpoolhub.com:12005 -u ${NAME} -p x) \
                   1> >(tee >(grep -q "Press any key to exit" && kill $(cat $PIDFILE)) >&1) \
               & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
 (PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
-         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --shaders ${SHADERS} --event-on idle --event-quit true -k neoscrypt -o stratum+tcp://hub.miningpoolhub.com:12012 -u ${NAME} -p x) \
+         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --shaders ${SHADERS} -K kernel --event-on idle --event-quit true -k neoscrypt -o stratum+tcp://hub.miningpoolhub.com:12012 -u ${NAME} -p x) \
                   1> >(tee >(grep -q "Press any key to exit" && kill $(cat $PIDFILE)) >&1) \
               & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
 (PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
-         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --shaders ${SHADERS} --event-on idle --event-quit true -k quarkcoin -o stratum+tcp://hub.miningpoolhub.com:12015 -u ${NAME} -p x) \
+         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --shaders ${SHADERS} -K kernel --event-on idle --event-quit true -k quarkcoin -o stratum+tcp://hub.miningpoolhub.com:12015 -u ${NAME} -p x) \
                   1> >(tee >(grep -q "Press any key to exit" && kill $(cat $PIDFILE)) >&1) \
               & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
 (PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
-         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --shaders ${SHADERS} --event-on idle --event-quit true -k qubitcoin -o stratum+tcp://hub.miningpoolhub.com:12014 -u ${NAME} -p x) \
+         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --shaders ${SHADERS} -K kernel --event-on idle --event-quit true -k qubitcoin -o stratum+tcp://hub.miningpoolhub.com:12014 -u ${NAME} -p x) \
                   1> >(tee >(grep -q "Press any key to exit" && kill $(cat $PIDFILE)) >&1) \
               & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
 (PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
@@ -58,19 +58,19 @@ ${CURDIR}/ethminer/ethminer -G -S us-east.ethash-hub.miningpoolhub.com:12020 -O 
                   1> >(tee >(grep -q "Press any key to exit" && kill $(cat $PIDFILE)) >&1) \
               & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
 (PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
-         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --shaders ${SHADERS} --event-on idle --event-quit true -k darkcoin-mod -o stratum+tcp://hub.miningpoolhub.com:12007 -u ${NAME} -p x) \
+         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --shaders ${SHADERS} -K kernel --event-on idle --event-quit true -k darkcoin-mod -o stratum+tcp://hub.miningpoolhub.com:12007 -u ${NAME} -p x) \
                   1> >(tee >(grep -q "Press any key to exit" && kill $(cat $PIDFILE)) >&1) \
               & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
 (PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
-         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --shaders ${SHADERS} --event-on idle --event-quit true -k marucoin-mod -o stratum+tcp://hub.miningpoolhub.com:12008 -u ${NAME} -p x) \
+         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --shaders ${SHADERS} -K kernel --event-on idle --event-quit true -k marucoin-mod -o stratum+tcp://hub.miningpoolhub.com:12008 -u ${NAME} -p x) \
                   1> >(tee >(grep -q "Press any key to exit" && kill $(cat $PIDFILE)) >&1) \
               & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
 (PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
-         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --shaders ${SHADERS} --event-on idle --event-quit true -k bitblock -o stratum+tcp://hub.miningpoolhub.com:12009 -u ${NAME} -p x) \
+         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --shaders ${SHADERS} -K kernel --event-on idle --event-quit true -k bitblock -o stratum+tcp://hub.miningpoolhub.com:12009 -u ${NAME} -p x) \
                   1> >(tee >(grep -q "Press any key to exit" && kill $(cat $PIDFILE)) >&1) \
               & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
 (PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
-         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --shaders ${SHADERS} --event-on idle --event-quit true -k lyra2rev2 -o stratum+tcp://hub.miningpoolhub.com:12018 -u ${NAME} -p x) \
+         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --shaders ${SHADERS} -K kernel --event-on idle --event-quit true -k lyra2rev2 -o stratum+tcp://hub.miningpoolhub.com:12018 -u ${NAME} -p x) \
                   1> >(tee >(grep -q "Press any key to exit" && kill $(cat $PIDFILE)) >&1) \
               & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
 (PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
