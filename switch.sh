4fe62ad1c85f83dbe8bc9877919b3740a0c96ae4
revolux <op@amirrk.com>
2017-08-31 17:49:36 -0700

    Changed switch script to work with miners that do not auto-exit reliably

--- switch.sh
+++ switch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #MINER NAME
-NAME=amir.rig9
+NAME=amir.ethos2
 
 
 ## DONT TOUCH THESE
 CURDIR=`dirname $0`
-echo 'revolux123' | sudo -S `dirname $0`/overclock.sh
+#echo 'revolux123' | sudo -S `dirname $0`/overclock.sh
 ####
 
 ## YOU CAN CHANGE THIS FROM INFINITE LOOP TO 
@@ -17,28 +17,56 @@ echo 'revolux123' | sudo -S `dirname $0`/overclock.sh
 while [ 1 ];
 do
 #etHASH
-${CURDIR}/ethminer/ethminer -U -S us-east.ethash-hub.miningpoolhub.com:12020 -O ${NAME}:x --farm-retries 0 -FS exit
-
-#Groestl
-${CURDIR}/ccminer/ccminer -r 0 -a groestl -o stratum+tcp://hub.miningpoolhub.com:12004 -O ${NAME}:x
-
-#myr-gr
-${CURDIR}/ccminer/ccminer -r 0 -a myr-gr -o stratum+tcp://hub.miningpoolhub.com:12005 -O ${NAME}:x
-
-#Lyra2Rev2
-${CURDIR}/ccminer/ccminer -r 0 -a lyra2v2 -o stratum+tcp://hub.miningpoolhub.com:12018 -O ${NAME}:x 
+${CURDIR}/ethminer/ethminer -G -S us-east.ethash-hub.miningpoolhub.com:12020 -O ${NAME}:x --farm-retries 0 -FS exit
 
 #monero
 ${CURDIR}/monero/xmrMiner/build/xmrMiner -r 0 -R 4 -l 16x54 -o stratum+tcp://us-east.cryptonight-hub.miningpoolhub.com:12024 -O ${NAME}:x -D
 
-#Feathercoin (NEOSCRYPT)
-${CURDIR}/ccminer/ccminer -r 0 -a neoscrypt -o stratum+tcp://hub.miningpoolhub.com:12012 -O ${NAME}:x 
-
-#skein
-${CURDIR}/ccminer/ccminer -r 0 -a skein -o stratum+tcp://hub.miningpoolhub.com:12016 -O ${NAME}:x 
-
-#Equihash
-${CURDIR}/zcash/miner --eexit 3 --intensity 64 64 64 64 64 64 64 --cuda_devices 0 1 2 3 4 5 6 --templimit 80 --server us-east.equihash-hub.miningpoolhub.com --port 12023 --user ${NAME} --pass x
+(PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
+         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --event-on idle --event-quit true -k vanilla -o stratum+tcp://hub.miningpoolhub.com:12019 -u ${NAME} -p x -o exit) \
+                  1> >(tee >(grep -q "Press any key to exit" && kill $(cat $PIDFILE)) >&1) \
+              & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
+(PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
+         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --event-on idle --event-quit true -k groestlcoin -o stratum+tcp://hub.miningpoolhub.com:12004 -u ${NAME} -p x) \
+                  1> >(tee >(grep -q "Press any key to exit" && kill $(cat $PIDFILE)) >&1) \
+              & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
+(PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
+         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --event-on idle --event-quit true -k myriadcoin-groestl -o stratum+tcp://hub.miningpoolhub.com:12005 -u ${NAME} -p x) \
+                  1> >(tee >(grep -q "Press any key to exit" && kill $(cat $PIDFILE)) >&1) \
+              & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
+(PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
+         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --event-on idle --event-quit true -k neoscrypt -o stratum+tcp://hub.miningpoolhub.com:12012 -u ${NAME} -p x) \
+                  1> >(tee >(grep -q "Press any key to exit" && kill $(cat $PIDFILE)) >&1) \
+              & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
+(PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
+         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --event-on idle --event-quit true -k quarkcoin -o stratum+tcp://hub.miningpoolhub.com:12015 -u ${NAME} -p x) \
+                  1> >(tee >(grep -q "Press any key to exit" && kill $(cat $PIDFILE)) >&1) \
+              & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
+(PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
+         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --event-on idle --event-quit true -k qubitcoin -o stratum+tcp://hub.miningpoolhub.com:12014 -u ${NAME} -p x) \
+                  1> >(tee >(grep -q "Press any key to exit" && kill $(cat $PIDFILE)) >&1) \
+              & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
+(PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
+         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --event-on idle --event-quit true -k skeincoin -o stratum+tcp://hub.miningpoolhub.com:12016 -u ${NAME} -p x) \
+                  1> >(tee >(grep -q "Press any key to exit" && kill $(cat $PIDFILE)) >&1) \
+              & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
+(PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
+         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --event-on idle --event-quit true -k darkcoin-mod -o stratum+tcp://hub.miningpoolhub.com:12007 -u ${NAME} -p x) \
+                  1> >(tee >(grep -q "Press any key to exit" && kill $(cat $PIDFILE)) >&1) \
+              & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
+(PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
+         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --event-on idle --event-quit true -k marucoin-mod -o stratum+tcp://hub.miningpoolhub.com:12008 -u ${NAME} -p x) \
+                  1> >(tee >(grep -q "Press any key to exit" && kill $(cat $PIDFILE)) >&1) \
+              & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
+(PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
+         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --event-on idle --event-quit true -k bitblock -o stratum+tcp://hub.miningpoolhub.com:12009 -u ${NAME} -p x) \
+                  1> >(tee >(grep -q "Press any key to exit" && kill $(cat $PIDFILE)) >&1) \
+              & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
+(PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
+         && { (unbuffer ${CURDIR}/sgminer-gm/sgminer --event-on idle --event-quit true -k lyra2rev2 -o stratum+tcp://hub.miningpoolhub.com:12018 -u ${NAME} -p x) \
+                  1> >(tee >(grep -q "Press any key to exit" && kill $(cat $PIDFILE)) >&1) \
+              & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
+${CURDIR}/optiminer-zcash/optiminer-zcash -i 64 -s us-east.equihash-hub.miningpoolhub.com:12023 -u ${NAME} -p x -watchdog-timeout 15 -watchdog-cmd exit
 
 
 done
