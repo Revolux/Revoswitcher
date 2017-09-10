c6c43086f28852916ee11e5076cb9a406ec041fb
revolux <op@amirrk.com>
2017-09-10 01:49:01 -0700

    System will now auto-reboot correctly. Zcash miner throws 'Unknown Error' error. CCminers throw 'is CUda installed?'. We can use a different catch on different lines to carry out different actions

--- switch.sh
+++ switch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #MINER NAME
-NAME=amir.FRM2
+NAME=amir.FRM4
 
 
 ## DONT TOUCH THESE
@@ -57,7 +57,7 @@ ${CURDIR}/monero/xmrMiner/build/xmrMiner -r 0 -R 4 -l 16x54 -o stratum+tcp://us-
 (PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
          && { (unbuffer ${CURDIR}/zcash/miner --eexit 3 --intensity 64 64 64 64 64 64 64 --cuda_devices 0 1 2 3 4 5 6 --templimit 80 --server us-east.equihash-hub.miningpoolhub.com --port 12023 --user ${NAME} --pass x) \
                   1> >(tee >(grep -q "Total speed: 0 Sol/s" && kill $(cat $PIDFILE)) >&1) \
-              & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; }) 1> >(tee >(grep -q "Unable to query number of CUDA devices! Is an nVidia driver installed?" && (echo 'revolux123' | sudo -S reboot) >&1))
+              & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; }) 1> >(tee >(grep -q "ERROR: unknown error" && (echo 'revolux123' | sudo -S reboot) >&1))
 done
 
 exit 0
