e6032dade0273b4462bc1dc66b39f618e53e664f
Amir Khazaieli <op@amirrk.com>
2017-11-13 03:10:24 -0800

    BTG mining

--- switch.sh
+++ switch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #MINER NAME
-NAME=amir.FRM2
+NAME=FRM2
 
 
 ## DONT TOUCH THESE
@@ -18,54 +18,54 @@ while [ 1 ];
 do
 
 #BTCZ
-(PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
-         && { (unbuffer ${CURDIR}/zcash/miner --eexit 3 --intensity 64 --cuda_devices 0 1 2 3 4 5 6 --templimit 80 --server btcz.suprnova.cc --port 5587 --user ak.rig3 --pass x) \
-                  1> >(tee >(grep -q "Total speed: 0 Sol/s" && kill $(cat $PIDFILE)) >&1) \
-              & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; }) 1> >(tee >(grep -q "ERROR: unknown error" && (echo 'revolux123' | sudo -S reboot) >&1))
+# (PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
+         # && { (unbuffer ${CURDIR}/zcash/miner --eexit 3 --intensity 64 --cuda_devices 0 1 2 3 4 5 6 --templimit 80 --server btcz.suprnova.cc --port 5587 --user ak.${NAKE} --pass x) \
+                  # 1> >(tee >(grep -q "Total speed: 0 Sol/s" && kill $(cat $PIDFILE)) >&1) \
+              # & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; }) 1> >(tee >(grep -q "ERROR: unknown error" && (echo 'revolux123' | sudo -S reboot) >&1))
 
-#etHASH
-(PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
-         && { (unbuffer ${CURDIR}/ethminer/ethminer -U -S us-east.ethash-hub.miningpoolhub.com:12020 -O ${NAME}:x --farm-retries 0 -FS exit) \
-                  1> >(tee >(grep -q "unspecified launch failure" && kill $(cat $PIDFILE)) >&1) \
-              & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
+# #etHASH
+# (PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
+         # && { (unbuffer ${CURDIR}/ethminer/ethminer -U -S us-east.ethash-hub.miningpoolhub.com:12020 -O ${NAME}:x --farm-retries 0 -FS exit) \
+                  # 1> >(tee >(grep -q "unspecified launch failure" && kill $(cat $PIDFILE)) >&1) \
+              # & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
 
-#Groestl
-(PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
-         && { (unbuffer ${CURDIR}/ccminer/ccminer -r 0 -a groestl -o stratum+tcp://hub.miningpoolhub.com:12004 -O ${NAME}:x) \
-                  1> >(tee >(grep -q "...terminating workio thread" && kill $(cat $PIDFILE)) >&1) \
-              & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
+# #Groestl
+# (PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
+         # && { (unbuffer ${CURDIR}/ccminer/ccminer -r 0 -a groestl -o stratum+tcp://hub.miningpoolhub.com:12004 -O ${NAME}:x) \
+                  # 1> >(tee >(grep -q "...terminating workio thread" && kill $(cat $PIDFILE)) >&1) \
+              # & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
 
-#myr-gr
-(PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
-         && { (unbuffer ${CURDIR}/ccminer/ccminer -r 0 -a myr-gr -o stratum+tcp://hub.miningpoolhub.com:12005 -O ${NAME}:x) \
-                  1> >(tee >(grep -q "...terminating workio thread" && kill $(cat $PIDFILE)) >&1) \
-              & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
+# #myr-gr
+# (PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
+         # && { (unbuffer ${CURDIR}/ccminer/ccminer -r 0 -a myr-gr -o stratum+tcp://hub.miningpoolhub.com:12005 -O ${NAME}:x) \
+                  # 1> >(tee >(grep -q "...terminating workio thread" && kill $(cat $PIDFILE)) >&1) \
+              # & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
 
-#Lyra2Rev2
-(PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
-         && { (unbuffer ${CURDIR}/ccminer/ccminer -r 0 -a lyra2v2 -o stratum+tcp://hub.miningpoolhub.com:12018 -O ${NAME}:x) \
-                  1> >(tee >(grep -q "...terminating workio thread" && kill $(cat $PIDFILE)) >&1) \
-              & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
+# #Lyra2Rev2
+# (PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
+         # && { (unbuffer ${CURDIR}/ccminer/ccminer -r 0 -a lyra2v2 -o stratum+tcp://hub.miningpoolhub.com:12018 -O ${NAME}:x) \
+                  # 1> >(tee >(grep -q "...terminating workio thread" && kill $(cat $PIDFILE)) >&1) \
+              # & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; })
 
 
-#monero
-${CURDIR}/monero/xmrMiner/build/xmrMiner -r 0 -R 4 -l 16x54 -o stratum+tcp://us-east.cryptonight-hub.miningpoolhub.com:12024 -O ${NAME}:x -D
+# #monero
+# ${CURDIR}/monero/xmrMiner/build/xmrMiner -r 0 -R 4 -l 16x54 -o stratum+tcp://us-east.cryptonight-hub.miningpoolhub.com:12024 -O ${NAME}:x -D
 
-#Feathercoin (NEOSCRYPT)
-(PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
-         && { (unbuffer ${CURDIR}/ccminer/ccminer -r 0 -a neoscrypt -o stratum+tcp://hub.miningpoolhub.com:12012 -O ${NAME}:x) \
-                  1> >(tee >(grep -q "...terminating workio thread" && kill $(cat $PIDFILE)) >&1) \
-              & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; }) 
+# #Feathercoin (NEOSCRYPT)
+# (PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
+         # && { (unbuffer ${CURDIR}/ccminer/ccminer -r 0 -a neoscrypt -o stratum+tcp://hub.miningpoolhub.com:12012 -O ${NAME}:x) \
+                  # 1> >(tee >(grep -q "...terminating workio thread" && kill $(cat $PIDFILE)) >&1) \
+              # & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; }) 
 
-#skein
-(PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
-         && { (unbuffer ${CURDIR}/ccminer/ccminer -r 0 -a skein -o stratum+tcp://hub.miningpoolhub.com:12016 -O ${NAME}:x ) \
-                  1> >(tee >(grep -q "...terminating workio thread" && kill $(cat $PIDFILE)) >&1) \
-              & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; }) 
+# #skein
+# (PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
+         # && { (unbuffer ${CURDIR}/ccminer/ccminer -r 0 -a skein -o stratum+tcp://hub.miningpoolhub.com:12016 -O ${NAME}:x ) \
+                  # 1> >(tee >(grep -q "...terminating workio thread" && kill $(cat $PIDFILE)) >&1) \
+              # & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; }) 
 
 #Equihash
 (PIDFILE=$(mktemp /tmp/foo.XXXXXX) && trap "rm $PIDFILE" 0 \
-         && { (unbuffer ${CURDIR}/zcash/miner --eexit 3 --intensity 64 64 64 64 64 64 64 --cuda_devices 0 1 2 3 4 5 6 --templimit 80 --server us-east.equihash-hub.miningpoolhub.com --port 12023 --user ${NAME} --pass x) \
+         && { (unbuffer ${CURDIR}/zcash/miner --eexit 3 --intensity 64 --cuda_devices 0 1 2 3 4 5 6 --templimit 80 --server btg.suprnova.cc --port 8816 --user ak.${NAME} --pass x) \
                   1> >(tee >(grep -q "Total speed: 0 Sol/s" && kill $(cat $PIDFILE)) >&1) \
               & PID=$! && echo $PID >$PIDFILE ; wait $PID || true; }) 1> >(tee >(grep -q "ERROR: unknown error" && (echo 'revolux123' | sudo -S reboot) >&1))
 done
